f05f90baace8213b31e5ba2e7fa4d2c1b599a497
Luc Georges <luc@huggingface.co>
2024-05-14 18:47:08 +0200

    feat: parallelised download

--- src/main.zig
+++ src/main.zig
@@ -16,15 +16,15 @@ const DownloadError = error{
 };
 
 const SharedContext = struct {
-    allocator: *mem.Allocator,
+    allocator: *const mem.Allocator,
     client: *http.Client,
     filename: []const u8,
-    headers: *http.Headers,
+    headers: *const []const http.Header,
     semaphore: *Thread.Semaphore,
-    uri: *Uri,
+    uri: *const Uri,
 };
 
-fn download(url: []const u8, filename: []const u8, max_file_handles: u16, chunk_size: u32, max_retries: u16, headers: *http.Headers) !void {
+fn download(url: []const u8, filename: []const u8, max_file_handles: u16, chunk_size: u32, max_retries: u16, headers: []const http.Header) !void {
     _ = max_retries;
     var gpa = heap.GeneralPurposeAllocator(.{}){};
     defer assert(gpa.deinit() == .ok);
@@ -34,19 +34,45 @@ fn download(url: []const u8, filename: []const u8, max_file_handles: u16, chunk_
     defer client.deinit();
 
     const uri = try Uri.parse(url);
-    var content_range_headers = http.Headers{ .allocator = allocator, .list = try headers.list.clone(allocator), .index = try headers.index.clone(allocator) };
-    defer content_range_headers.deinit();
-    try content_range_headers.append("Range", "bytes=0-0");
-    var request = try client.request(.GET, uri, content_range_headers, .{});
-    try request.start();
-    try request.wait();
+    var headers_with_content_range = std.ArrayList(http.Header).init(allocator);
+    defer headers_with_content_range.deinit();
+    try headers_with_content_range.appendSlice(headers);
+    try headers_with_content_range.append(http.Header{ .name = "Range", .value = "bytes=0-0" });
+    var server_header_buffer: [16 * 1024]u8 = undefined;
+    var req = try client.open(.GET, uri, .{
+        .headers = .{ .authorization = .omit },
+        .extra_headers = headers_with_content_range.items,
+        .server_header_buffer = &server_header_buffer,
+    });
 
-    if (request.response.status != http.Status.ok) {
-        std.debug.print("[get_range] API response error, replied with status: {d}", .{request.response.status});
+    try req.send();
+    try req.finish();
+    try req.wait();
+
+    assert(try req.response.parser.read(req.connection.?, &.{}, true) == 0);
+
+    if (req.response.status != http.Status.partial_content) {
+        std.log.err("[get_range] API response error, replied with status: {d}", .{req.response.status});
         return DownloadError.InvalidResponseStatus;
     }
 
-    const content_range = request.response.headers.getFirstValue("Content-range") orelse return DownloadError.MissingContentRange;
+    var header_iter = http.HeaderIterator.init(&server_header_buffer);
+    var found = false;
+    var content_range: []const u8 = undefined;
+    while (header_iter.next()) |header| {
+        if (std.ascii.eqlIgnoreCase(header.name, "Content-range")) {
+            found = true;
+            content_range = header.value;
+            break;
+        }
+    }
+
+    if (!found) {
+        return DownloadError.MissingContentRange;
+    }
+
+    req.deinit();
+
     var it = mem.split(u8, content_range, "/");
     var skip_first = true;
     var length: usize = undefined;
@@ -55,10 +81,8 @@ fn download(url: []const u8, filename: []const u8, max_file_handles: u16, chunk_
             skip_first = false;
             continue;
         }
-        length = try std.fmt.parseInt(u8, chunk, 10);
+        length = try std.fmt.parseInt(usize, chunk, 10);
     }
-
-    request.deinit();
 
     var pool: Thread.Pool = undefined;
     try pool.init(.{ .n_jobs = max_file_handles, .allocator = allocator });
@@ -66,10 +90,11 @@ fn download(url: []const u8, filename: []const u8, max_file_handles: u16, chunk_
 
     var semaphore = Thread.Semaphore{ .permits = max_file_handles };
 
-    var ctx = SharedContext{
+    const ctx = SharedContext{
+        .allocator = &allocator,
         .client = &client,
         .filename = filename,
-        .headers = headers,
+        .headers = &headers,
         .semaphore = &semaphore,
         .uri = &uri,
     };
@@ -80,7 +105,7 @@ fn download(url: []const u8, filename: []const u8, max_file_handles: u16, chunk_
         try pool.spawn(
             downloadChunk,
             .{
-                ctx,
+                &ctx,
                 start,
                 stop,
             },
@@ -90,51 +115,78 @@ fn download(url: []const u8, filename: []const u8, max_file_handles: u16, chunk_
     return;
 }
 
-fn downloadChunk(ctx: *SharedContext, start: usize, stop: usize) void {
+fn downloadChunk(ctx: *const SharedContext, start: usize, stop: usize) void {
     ctx.semaphore.wait();
-    const range = try std.fmt.allocPrint(ctx.allocator.*, "bytes={d}-{d}", .{ start, stop });
+    const range = std.fmt.allocPrint(ctx.allocator.*, "bytes={d}-{d}", .{ start, stop }) catch {
+        std.log.err("[OOM] failed to allocate memory for range header", .{});
+        return;
+    };
+    std.log.info("querying range: {s}", .{range});
     defer ctx.allocator.*.free(range);
-    var request = try ctx.client.request(.GET, ctx.uri.*, ctx.headers.*, .{});
+    var server_header_buffer: [16 * 1024]u8 = undefined;
+    var request = ctx.client.open(.GET, ctx.uri.*, .{
+        .server_header_buffer = &server_header_buffer,
+        .headers = .{ .authorization = .omit },
+        .extra_headers = ctx.headers.*,
+    }) catch |err| {
+        std.log.err("error: {s}", .{@errorName(err)});
+        return;
+    };
     defer request.deinit();
-    try request.start();
-    try request.wait();
+    request.send() catch |err| {
+        std.log.err("error: {s}", .{@errorName(err)});
+        return;
+    };
+    request.wait() catch |err| {
+        std.log.err("error: {s}", .{@errorName(err)});
+        return;
+    };
 
     if (request.response.status != http.Status.ok) {
-        std.debug.print("[chunk_download] API response error, replied with status: {d}", .{request.response.status});
+        std.log.err("[chunk_download] API response error, replied with status: {d}", .{request.response.status});
         ctx.semaphore.post();
-        return DownloadError.InvalidResponseStatus;
+        return;
     }
 
-    var file = if (ctx.filename.len > 0 and ctx.filename[0] == '/') try fs.createFileAbsolute(ctx.filename, .{ .mode = 0o644, .truncate = false }) else try fs.cwd().createFile(ctx.filename, .{ .mode = 0o644, .truncate = false });
+    var file = if (ctx.filename.len > 0 and ctx.filename[0] == '/') fs.createFileAbsolute(ctx.filename, .{ .mode = 0o644, .truncate = false }) catch |err| {
+        std.log.err("error: {s}", .{@errorName(err)});
+        return;
+    } else fs.cwd().createFile(ctx.filename, .{ .mode = 0o644, .truncate = false }) catch |err| {
+        std.log.err("error: {s}", .{@errorName(err)});
+        return;
+    };
     defer file.close();
 
     var buffer: [4096]u8 = undefined;
     var read_bytes: usize = 1;
 
     while (read_bytes > 0) {
-        read_bytes = try request.reader().read(buffer[0..]);
-        try file.writeAll(buffer[0..read_bytes]);
+        read_bytes = request.reader().read(buffer[0..]) catch |err| {
+            std.log.err("error: {s}", .{@errorName(err)});
+            return;
+        };
+        file.writeAll(buffer[0..read_bytes]) catch |err| {
+            std.log.err("error: {s}", .{@errorName(err)});
+            return;
+        };
     }
     ctx.semaphore.post();
 }
 
 pub fn main() !void {
-    var gpa = heap.GeneralPurposeAllocator(.{}){};
-    defer assert(gpa.deinit() == .ok);
-    const allocator = gpa.allocator();
-    var headers = http.Headers.init(allocator);
-    defer headers.deinit();
-    const filename = "/tmp/eicar_test_file";
+    var headers = [_]http.Header{};
+    // const filename = "/tmp/eicar_test_file";
+    const filename = "/tmp/model.safetensors";
 
-    try download("https://huggingface.co/mcpotato/42-eicar-street/resolve/main/eicar_test_file", filename, 16, 10_485_760, 5, &headers);
+    // try download("https://huggingface.co/mcpotato/42-eicar-street/resolve/main/eicar_test_file", filename, 16, 10_485_760, 5, &headers);
+    try download("https://huggingface.co/google-bert/bert-base-uncased/resolve/main/model.safetensors", filename, 16, 10_485_760, 5, &headers);
 }
 
 test "simple download" {
     var gpa = heap.GeneralPurposeAllocator(.{}){};
     defer assert(gpa.deinit() == .ok);
     const allocator = gpa.allocator();
-    var headers = http.Headers.init(allocator);
-    defer headers.deinit();
+    const headers = [_]http.Header{};
     const filename = "eicar_test_file";
 
     try download("https://huggingface.co/mcpotato/42-eicar-street/resolve/main/eicar_test_file", filename, 16, 10_485_760, 5, &headers);
